37265b41407f9a553b7323e97ff371ba668de450
Yuchen Deng <loaden@gmail.com>
2022-02-16 18:12:42 +0800

    添加Garuda内核配置

--- sync.sh
+++ sync.sh
@@ -10,6 +10,7 @@ cp -r /etc/portage/repos.conf/ etc/portage/
 cp -r /etc/portage/package.accept_keywords/ etc/portage/
 cp -r /etc/portage/package.mask/ etc/portage/
 cp -r /etc/portage/package.use/ etc/portage/
+rm -rf etc/dracut.conf.d/
 mkdir etc/dracut.conf.d/
 cp -r /etc/dracut.conf.d/ etc/
 
@@ -18,5 +19,4 @@ sudo cat /var/lib/portage/world > var/lib/portage/world
 
 # usr
 cp /usr/src/linux/.config usr/src/linux/
-cp /usr/src/linux/.config.ubuntu.* usr/src/linux/
-cp /usr/src/linux/.config.gentoo.* usr/src/linux/
+cp /usr/src/linux/.config.*.* usr/src/linux/
