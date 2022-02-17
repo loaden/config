284a75b8b6296d84f1445141d3bc7a01f7547741
Yuchen Deng <loaden@gmail.com>
2022-02-17 19:06:34 +0800

    不应该禁用X

--- sync.sh
+++ sync.sh
@@ -4,16 +4,17 @@
 
 # portage
 [ -d /etc/portage/ ] && rm -rf etc/portage/
-mkdir etc/portage
+mkdir -p etc/portage
 cp /etc/portage/make.conf etc/portage/make.conf
-cp -r /etc/portage/repos.conf/ etc/portage/
 cp -r /etc/portage/package.accept_keywords/ etc/portage/
+rm -f etc/portage/package.accept_keywords/zz-*
 cp -r /etc/portage/package.mask/ etc/portage/
 cp -r /etc/portage/package.use/ etc/portage/
+rm -f etc/portage/package.use/zz-*
 
 # dracut
 [ -d /etc/dracut.conf.d/ ] && rm -rf etc/dracut.conf.d/
-mkdir etc/dracut.conf.d/
+mkdir -p etc/dracut.conf.d/
 cp -r /etc/dracut.conf.d/ etc/
 
 # kernel config
