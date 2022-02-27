1162ab9080350f40db17cad8a23771cc403dc9dc
Yuchen Deng <loaden@gmail.com>
2022-02-27 22:23:55 +0800

    添加字体配置脚本

--- etc/config.sh
+++ etc/config.sh
@@ -8,9 +8,13 @@ sudo systemctl preset-all --preset-mode=enable-only
 # sudo systemctl --global enable pipewire.socket
 
 # 主机名
-read -p "Please input the hostname: " hostname
-sudo hostnamectl set-hostname $hostname
-[[ ! $(cat /etc/hosts | grep $hostname) ]] && sudo HOSTNAME=$hostname bash -c 'echo -e "\n127.0.0.1\t$HOSTNAME.me $HOSTNAME\n" >> /etc/hosts'
+if [ "$(hostname)" != "lucky" ]; then
+    read -p "Please input the hostname: " hostname
+    sudo hostnamectl set-hostname $hostname
+    if [[ ! $(cat /etc/hosts | grep $hostname) ]]; then
+        sudo HOSTNAME=$hostname bash -c 'echo -e "\n127.0.0.1\t$HOSTNAME.me $HOSTNAME\n" >> /etc/hosts'
+    fi
+fi
 
 # 时区
 sudo timedatectl set-timezone Asia/Shanghai
@@ -23,7 +27,8 @@ sudo bash -c 'echo -e "C.UTF8 UTF-8\nzh_CN.UTF-8 UTF-8\n" > /etc/locale.gen'
 sudo locale-gen
 sudo localectl set-locale LANG=zh_CN.utf8
 eselect locale list
-# eselect locale set <number>
+sudo eselect locale set zh_CN.utf8
+eselect locale list
 
 # 用户目录
 sudo emerge -avu xdg-user-dirs
@@ -56,3 +61,54 @@ sudo bash -c 'echo -e "[defaults]\nntfs_defaults=uid=$UID,gid=$GID,noatime,preal
 
 # 允许弱密码
 sudo sed -i 's/enforce=everyone/enforce=none/g' /etc/security/passwdqc.conf
+
+# 禁用字体配置
+sudo eselect fontconfig disable 10-hinting-slight.conf
+sudo eselect fontconfig disable 10-scale-bitmap-fonts.conf
+sudo eselect fontconfig disable 20-unhint-small-vera.conf
+sudo eselect fontconfig disable 30-metric-aliases.conf
+sudo eselect fontconfig disable 40-nonlatin.conf
+sudo eselect fontconfig disable 45-generic.conf
+sudo eselect fontconfig disable 45-latin.conf
+sudo eselect fontconfig disable 49-sansserif.conf
+sudo eselect fontconfig disable 50-user.conf
+sudo eselect fontconfig disable 51-local.conf
+sudo eselect fontconfig disable 60-generic.conf
+sudo eselect fontconfig disable 60-latin.conf
+sudo eselect fontconfig disable 65-fonts-persian.conf
+sudo eselect fontconfig disable 65-nonlatin.conf
+sudo eselect fontconfig disable 69-unifont.conf
+sudo eselect fontconfig disable 80-delicious.conf
+sudo eselect fontconfig disable 90-synthetic.conf
+
+# 启用字体配置
+sudo eselect fontconfig enable 11-lcdfilter-default.conf
+# sudo eselect fontconfig enable 10-hinting-slight.conf
+# sudo eselect fontconfig enable 10-scale-bitmap-fonts.conf
+# sudo eselect fontconfig enable 20-unhint-small-vera.conf
+# sudo eselect fontconfig enable 30-metric-aliases.conf
+# sudo eselect fontconfig enable 40-nonlatin.conf
+# sudo eselect fontconfig enable 45-generic.conf
+# sudo eselect fontconfig enable 45-latin.conf
+sudo eselect fontconfig enable 49-sansserif.conf
+sudo eselect fontconfig enable 50-user.conf
+# sudo eselect fontconfig enable 51-local.conf
+# sudo eselect fontconfig enable 60-generic.conf
+sudo eselect fontconfig enable 60-latin.conf
+# sudo eselect fontconfig enable 65-fonts-persian.conf
+# sudo eselect fontconfig enable 65-nonlatin.conf
+# sudo eselect fontconfig enable 69-unifont.conf
+# sudo eselect fontconfig enable 80-delicious.conf
+# sudo eselect fontconfig enable 90-synthetic.conf
+
+
+# 刷新字体缓存
+sudo -E fc-cache -rv
+
+# 检查字体匹配
+fc-match Arial
+fc-match --verbose sans-serif
+
+# 更新环境变量
+sudo env-update
+source /etc/profile
