3f861c23ddafe40ddd5a2616f52ca9988557e493
Yuchen Deng <loaden@gmail.com>
2022-03-03 14:14:53 +0800

    更新Arch配置脚本

--- .local/bin/arch-config.sh
+++ .local/bin/arch-config.sh
@@ -20,14 +20,14 @@ sudo reflector --verbose -c CN --protocol https --sort rate --latest 10 --save /
 sudo perl -0777 -pi -e 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/g' /etc/pacman.conf
 [[ ! $(cat /etc/pacman.conf | grep archlinuxcn) ]] && sudo bash -c 'echo -e "\n[archlinuxcn]\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf'
 sudo pacman -Syy
-sudo pacman -S archlinuxcn-keyring --noconfirm --needed
+sudo pacman -S archlinuxcn-keyring --noconfirm
+sudo pacman -S archlinux-keyring --noconfirm
 
 # 中文
 sudo sed -i 's/# en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 sudo sed -i 's/# zh_CN.UTF-8/zh_CN.UTF-8/g' /etc/locale.gen
 sudo locale-gen
 sudo localectl set-locale LANG=zh_CN.UTF-8
-sudo pacman -S noto-fonts-cjk --noconfirm --needed
 
 # 重启生效
-read -p "重启生效。" -n 1
+read -p "完事。" -n 1
