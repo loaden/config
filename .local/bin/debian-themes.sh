2efcbe25038c6333fbd03c2ea2e6254c6d4a8c81
Yuchen Deng <loaden@gmail.com>
2022-03-20 14:37:53 +0800

    debian图标主题按喜好排序与精简

--- .local/bin/debian-themes.sh
+++ .local/bin/debian-themes.sh
@@ -2,12 +2,9 @@
 # 维护：Yuchen Deng [loaden] 钉钉群：35948877
 # QQ群：19346666、111601117
 
+sudo apt install suru-icon-theme -y
 sudo apt install obsidian-icon-theme -y
 sudo apt install numix-icon-theme* -y
-sudo apt install breeze-icon-theme -y
-sudo apt install faba-icon-theme -y
-sudo apt install human-icon-theme -y
-sudo apt install gnome-wine-icon-theme -y
 sudo apt install gnome-wise-icon-theme -y
-sudo apt install gnome-illustrious-icon-theme -y
-sudo apt install suru-icon-theme -y
+sudo apt install gnome-wine-icon-theme -y
+sudo apt install faba-icon-theme -y
