019ef3ff63d50caad4d5386c3b81dc6ae2860108
Yuchen Deng <loaden@gmail.com>
2022-03-23 23:07:01 +0800

    添加Arch图标主题

--- .local/bin/arch-apps.sh
+++ .local/bin/arch-apps.sh
@@ -3,6 +3,7 @@
 
 # 桌面软件
 sudo pacman -Syy
+sudo pacman -S audacious --noconfirm --needed #音乐播放器
 sudo pacman -S bash-completion --noconfirm --needed #终端自动完成
 #sudo pacman -S cups ghostscript gsfonts hplip hplip-plugin --noconfirm --needed #打印机
 sudo pacman -S firefox firefox-i18n-zh-cn --noconfirm --needed #浏览器
@@ -11,7 +12,6 @@ sudo pacman -S gnome-disk-utility --noconfirm --needed #磁盘
 sudo pacman -S htop --noconfirm --needed #进程查看器
 sudo pacman -S inetutils --noconfirm --needed #telnet
 sudo pacman -S neofetch --noconfirm --needed #系统信息
-sudo pacman -S deadbeef --noconfirm --needed #音乐播放器
 sudo pacman -S fcitx5-im fcitx5-rime --noconfirm --needed #输入法
 sudo pacman -S gimp --noconfirm --needed #图像处理
 sudo pacman -S mpv --noconfirm --needed #视频播放器
@@ -23,6 +23,10 @@ sudo pacman -S gnome-tweaks --noconfirm --needed #优化
 # AUR软件
 yay -S visual-studio-code-bin --noconfirm --needed #Visual Studio Code
 yay -S dingtalk-bin --noconfirm --needed #钉钉
+
+# 图标
+sudo pacman -S papirus-icon-theme
+yay -S numix-icon-theme-git --noconfirm --needed
 
 # 远程连接
 sudo pacman -S openssh --noconfirm #远程连接
