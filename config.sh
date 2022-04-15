9b0bb654794bd9af130dee0a71db674a28e3a96b
Yuchen Deng <loaden@gmail.com>
2022-04-15 21:20:52 +0800

    同步master修改

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@
 # 启动单元初始化配置
 sudo systemd-machine-id-setup
 sudo systemctl preset-all --preset-mode=enable-only --now
+systemd-machine-id-setup
 systemctl --user preset-all --now
 
 # 主机名
@@ -34,12 +35,6 @@ eselect locale list
 sudo emerge -avu1 xdg-user-dirs
 xdg-user-dirs-update --force
 
-# 添加官方GURU源和中国用户源
-sudo emerge -avu eselect-repository
-sudo eselect repository enable gentoo-zh
-sudo eselect repository enable guru
-sudo emerge --sync
-
 # 清理未完成的安装任务
 sudo emaint --fix cleanresume
 
@@ -51,10 +46,14 @@ sudo systemctl enable acpid.service
 sudo systemctl enable thermald.service
 
 # 用户组
-sudo usermod -aG wheel,audio,video,input,lpadmin,plugdev,pcap $USER
+sudo usermod -aG users,audio,video $USER
+sudo usermod -aG lpadmin $USER
+sudo usermod -aG scanner $USER
+sudo usermod -aG plugdev $USER
+sudo usermod -aG pcap $USER
 
 # udisks 支持 NTFS3
-sudo bash -c 'echo -e "[defaults]\nntfs_defaults=uid=$SUDO_UID,gid=$SUDO_GID,noatime,prealloc" > /etc/udisks2/mount_options.conf'
+sudo bash -c 'echo -e "[defaults]\nntfs_defaults=uid=$UID,gid=$GID,noatime,prealloc" > /etc/udisks2/mount_options.conf'
 
 # 别名
 if [ -z "$(grep .bash_aliases ~/.bashrc)" ]; then
@@ -72,28 +71,38 @@ systemctl --user enable --now pipewire pipewire-pulse
 systemctl --user daemon-reload
 LANG=C pactl info | grep "Server Name"
 
+# 蓝牙
+sudo systemctl enable bluetooth --now
+bluetoothctl list
+
 # 重载UDEV规则
 sudo udevadm control --reload
 sudo udevadm trigger
 
+# 添加官方GURU源和中国用户源
+sudo emerge -avu eselect-repository
+sudo eselect repository enable gentoo-zh >/dev/null
+sudo eselect repository enable guru >/dev/null
+sudo emerge-webrsync
+
 # 禁用字体配置
-sudo eselect fontconfig disable 10-hinting-slight.conf
-sudo eselect fontconfig disable 10-scale-bitmap-fonts.conf
-sudo eselect fontconfig disable 20-unhint-small-vera.conf
-sudo eselect fontconfig disable 30-metric-aliases.conf
-sudo eselect fontconfig disable 40-nonlatin.conf
-sudo eselect fontconfig disable 45-generic.conf
-sudo eselect fontconfig disable 45-latin.conf
-sudo eselect fontconfig disable 49-sansserif.conf
-sudo eselect fontconfig disable 50-user.conf
-sudo eselect fontconfig disable 51-local.conf
-sudo eselect fontconfig disable 60-generic.conf
-sudo eselect fontconfig disable 60-latin.conf
-sudo eselect fontconfig disable 65-fonts-persian.conf
-sudo eselect fontconfig disable 65-nonlatin.conf
-sudo eselect fontconfig disable 69-unifont.conf
-sudo eselect fontconfig disable 80-delicious.conf
-sudo eselect fontconfig disable 90-synthetic.conf
+sudo eselect fontconfig disable 10-hinting-slight.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 10-scale-bitmap-fonts.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 20-unhint-small-vera.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 30-metric-aliases.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 40-nonlatin.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 45-generic.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 45-latin.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 49-sansserif.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 50-user.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 51-local.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 60-generic.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 60-latin.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 65-fonts-persian.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 65-nonlatin.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 69-unifont.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 80-delicious.conf >/dev/null 2>&1
+sudo eselect fontconfig disable 90-synthetic.conf >/dev/null 2>&1
 
 # 启用字体配置
 sudo eselect fontconfig enable 40-nonlatin.conf
