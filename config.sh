624a524daa1e3eb5720563bfc5736b5938fbc7fc
Yuchen Deng <loaden@gmail.com>
2022-06-10 23:36:51 +0800

    Update config

--- config.sh
+++ config.sh
@@ -2,15 +2,14 @@
 # 维护：Yuchen Deng [loaden] 钉钉群：35948877
 # QQ群：19346666、111601117
 
-# 仅允许普通用户权限执行
-if [ $EUID == 0 ]; then
-    echo $(basename $0) 命令只允许普通用户执行
-    exit 1
-fi
-
 # 启动单元初始化配置
 sudo systemd-firstboot --setup-machine-id
-sudo systemctl preset-all
+sudo systemctl preset-all --preset-mode=enable-only
+
+# 网络
+sudo systemctl disable --now systemd-networkd.socket systemd-networkd.service
+sudo systemctl disable --now systemd-resolved.service
+sudo systemctl enable --now NetworkManager.service
 
 # 主机名
 HOSTNAME=lucky
@@ -33,12 +32,8 @@ eselect locale list
 sudo eselect locale set zh_CN.utf8
 eselect locale list
 
-# 用户目录
-sudo emerge -avu1 xdg-user-dirs
-xdg-user-dirs-update --force
-
-# 清理未完成的安装任务
-sudo emaint --fix cleanresume
+# 允许弱密码
+sudo sed -i 's/enforce=everyone/enforce=none/g' /etc/security/passwdqc.conf
 
 # 配置默认终端
 sudo chsh -s /bin/bash $USER
@@ -53,41 +48,48 @@ sudo usermod -aG lpadmin $USER
 #sudo usermod -aG scanner $USER
 sudo usermod -aG pcap $USER
 
-# udisks 支持 NTFS3
-#sudo bash -c 'echo -e "[defaults]\nntfs_defaults=uid=$UID,gid=$GID,noatime,prealloc" > /etc/udisks2/mount_options.conf'
+# 支持NTFS3
+sudo bash -c 'echo -e "[defaults]\nntfs_defaults=uid=0,gid=0,noatime,prealloc" > /etc/udisks2/mount_options.conf'
+
+# 蓝牙
+# sudo systemctl enable bluetooth --now
+# bluetoothctl list
+
+# 添加中国用户源和官方GURU源
+sudo emerge -avu1 eselect-repository
+sudo eselect repository enable gentoo-zh >/dev/null
+# sudo eselect repository enable guru >/dev/null
+# emerge --sync
+
+
+########################################################
+# 从现在开始仅允许普通用户权限执行                     #
+########################################################
+if [ $EUID == 0 ]; then
+    echo $(basename $0) 命令从现在开始只允许普通用户执行
+    exit 1
+fi
+
+# 用户目录
+sudo emerge -avu1 xdg-user-dirs
+xdg-user-dirs-update --force
 
 # 别名
 if [ -z "$(grep .bash_aliases ~/.bashrc)" ]; then
     echo "[ -f ~/.bash_aliases ] && . ~/.bash_aliases" >> ~/.bashrc
 fi
 
-# 允许弱密码
-sudo sed -i 's/enforce=everyone/enforce=none/g' /etc/security/passwdqc.conf
-
 # PipeWire替代PulseAudio
-#sudo sed -i 's/.*autospawn =.*/autospawn = no/g' /etc/pulse/client.conf
-#sudo sed -i 's/.*daemonize =.*/daemonize = no/g' /etc/pulse/daemon.conf
-#systemctl --user disable --now pulseaudio.service pulseaudio.socket
-#systemctl --user enable --now pipewire.socket pipewire-pulse.socket
-#systemctl --user daemon-reload
+sudo sed -i 's/.*autospawn =.*/autospawn = no/g' /etc/pulse/client.conf
+sudo sed -i 's/.*daemonize =.*/daemonize = no/g' /etc/pulse/daemon.conf
+systemctl --user disable --now pulseaudio.service pulseaudio.socket
+systemctl --user enable --now pipewire.socket pipewire-pulse.socket
+systemctl --user daemon-reload
 LANG=C pactl info | grep "Server Name"
 
 # PipeWire更换session服务
-#systemctl --user disable pipewire-media-session.service
-#systemctl --user --force enable wireplumber.service
-
-# 蓝牙
-# sudo systemctl enable bluetooth --now
-# bluetoothctl list
-
-# 重载UDEV规则
-sudo udevadm control --reload
-sudo udevadm trigger
-
-# 添加官方GURU源和中国用户源
-sudo emerge -avu eselect-repository
-sudo eselect repository enable gentoo-zh >/dev/null
-sudo eselect repository enable guru >/dev/null
+systemctl --user disable --now pipewire-media-session.service
+systemctl --user enable --now --force wireplumber.service
 
 # 禁用字体配置
 sudo eselect fontconfig disable 10-hinting-slight.conf >/dev/null 2>&1
