4f48270fbfc32e6e4d52cb35f678efddeb7d76d0
Yuchen Deng <loaden@gmail.com>
2022-06-13 23:22:55 +0800

    添加timeshift

--- config.sh
+++ config.sh
@@ -62,9 +62,8 @@ sudo systemctl restart polkit.service
 # 配置中国用户源和官方GURU源
 sudo emerge -avu1 eselect-repository
 sudo eselect repository enable gentoo-zh >/dev/null
-# sudo eselect repository enable guru >/dev/null
-sudo eselect repository disable guru >/dev/null
-# emerge --sync
+sudo eselect repository enable guru >/dev/null
+sudo eix-update
 
 
 ########################################################
